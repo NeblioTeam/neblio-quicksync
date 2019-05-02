b8cddbfa0c6ffebb3345e4492dca175bf07c17d0
NeblioTeam <neblioteam@gmail.com>
2019-05-02 18:05:50 -0400

    Add QuickSync Script

---
+++ quicksync.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# abort on error
+set -e
+
+# clone and build our repo
+git clone -b QuickSyncAll https://github.com/NeblioTeam/neblio
+cd neblio
+python ci_scripts/test_linux-daemon.py
+
+echo "Finished Building. Syncing blockchain data"
+mkdir -p $HOME/.neblio
+
+cat <<EOF > $HOME/.neblio/neblio.conf
+rpcuser=${RPCUSER:-nebliorpc}
+rpcpassword=${RPCPASSWORD:-rpctemp}
+EOF
+
+# start nebliod, which will kick off QuickSync
+./nebliod -daemon=1
+
+# sync the remainder of the chain
+i=0
+while true
+do
+    (( i = i + 1 ))
+    REMOTE_COUNT=`wget -O - http://explorer.nebl.io/api/getblockcount 2>/dev/null`
+    LOCAL_COUNT=`nebliod getblockcount 2>&1`
+    echo "Syncing $LOCAL_COUNT \ $REMOTE_COUNT"
+    if [ "$LOCAL_COUNT" -eq "$REMOTE_COUNT" ]; then
+        break
+    fi
+    if [ "$i" -gt 60 ]; then
+        # break after 30 minutes no matter what
+        break
+    fi
+    sleep 30
+done
+sleep 10
+./nebliod stop
+sleep 10
+rm $HOME/.neblio/neblio.conf
+rm $HOME/.neblio/wallet.dat
+# move our db files out for deployment
+mv $HOME/.neblio/txlmdb $HOME/$TRAVIS_BUILD_DIR/
