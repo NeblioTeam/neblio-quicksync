c48af9847715aa8c0cf5d0556d970d2501e9722d
NeblioTeam <neblioteam@gmail.com>
2019-11-14 18:04:22 -0500

    Fix typo and add GHA job restart

--- verify_deploy.py
+++ verify_deploy.py
@@ -1,13 +1,21 @@
 import os, json, hashlib, requests, subprocess, time, sys
 
 def restart_job():
-   print('FAILURE. RESTARTING JOB.')
+    print('FAILURE. RESTARTING JOB.')
 
-   url = 'https://api.travis-ci.org/repo/NeblioTeam%2Fneblio-quicksync/requests'
-   token = "token " + os.environ["TRAVIS_API_TOKEN"]
-   h = {'Content-Type': 'application/json', 'Travis-API-Version': '3', 'Accept': 'application/json', 'Authorization': token}
-   d = {}
-   r = requests.post(url, data=d, headers=h)
+    if os.environ.get('TRAVIS_API_TOKEN') is not None:
+        url = 'https://api.travis-ci.org/repo/NeblioTeam%2Fneblio-quicksync/requests'
+        token = "token " + os.environ["TRAVIS_API_TOKEN"]
+        h = {'Content-Type': 'application/json', 'Travis-API-Version': '3', 'Accept': 'application/json', 'Authorization': token}
+        d = {}
+        r = requests.post(url, data=d, headers=h)
+    else:
+        url = 'https://api.github.com/repos/NeblioTeam/neblio-quicksync/dispatches'
+        token = "token " + os.environ["GITHUB_TOKEN"]
+        h = {'Accept': 'application/vnd.github.everest-preview+json', 'Authorization': token}
+        d = {"event_type": "restart_neblio_quicksync"}
+        r = requests.post(url, data=json.dumps(d), headers=h)
+
 
    print('Deploy Verification Failed. Killing This Job.')
    sys.exit(1)
@@ -40,12 +48,12 @@ print(data_sha256)
 tmp_dir = 'tmp_download'
 prefix = ''
 if not os.path.exists(tmp_dir):
-	# check first download
-	prefix = "https://quicksync.ams3.digitaloceanspaces.com/txlmdb/"
-	os.mkdir(tmp_dir) # dir does not exist, create it
+    # check first download
+    prefix = "https://quicksync.ams3.digitaloceanspaces.com/txlmdb/"
+    os.mkdir(tmp_dir) # dir does not exist, create it
 else:
-	# check second download
-	prefix = "https://quicksync-backup.sfo2.digitaloceanspaces.com/txlmdb/"
+    # check second download
+    prefix = "https://quicksync-backup.sfo2.digitaloceanspaces.com/txlmdb/"
 
 os.chdir(tmp_dir)
 downloaded_sha256 = ''
@@ -55,15 +63,15 @@ url2 = prefix + os.environ['COMMIT'] + "/lock.mdb"
 for url in [url1, url2]:
     check = requests.head(url)
     if check.status_code > 399:
-    	# RESTART JOB
-    	restart_job()
+        # RESTART JOB
+        restart_job()
     # URL exists, download file
     print('Downloading URL to verify checksum: ' + url)
     file_name = url.rsplit('/', 1)[1]
     r = requests.get(url, allow_redirects=True)
     if r.status_code > 399:
-    	# RESTART JOB
-    	restart_job()
+        # RESTART JOB
+        restart_job()
     open(file_name, 'wb').write(r.content)
     # verify checksum
     sha256_hash = hashlib.sha256()
@@ -74,27 +82,27 @@ for url in [url1, url2]:
     downloaded_sha256 = sha256_hash.hexdigest()
     # verify checksums
     if file_name == "data.mdb":
-    	if data_sha256 != downloaded_sha256:
-    		print('SHA256 did not match for data.mdb')
-    		print('Original SHA256: ' + data_sha256)
-    		print('Download SHA256: ' + downloaded_sha256)
-    		# RESTART JOB
-    		restart_job()
-    	else:
-    		# checksum valid
-    		print(file_name + " sha256sum is valid")
-    		os.remove(file_name)
+        if data_sha256 != downloaded_sha256:
+            print('SHA256 did not match for data.mdb')
+            print('Original SHA256: ' + data_sha256)
+            print('Download SHA256: ' + downloaded_sha256)
+            # RESTART JOB
+            restart_job()
+        else:
+            # checksum valid
+            print(file_name + " sha256sum is valid")
+            os.remove(file_name)
     if file_name == "lock.mdb":
-    	if lock_sha256 != downloaded_sha256:
-    		print('SHA256 did not match for lock.mdb')
-    		print('Original SHA256: ' + data_sha256)
-    		print('Download SHA256: ' + downloaded_sha256)
-    		# RESTART JOB
-    		restart_job()
-    	else:
-    		# checksum valid
-    		print(file_name + " sha256sum is valid")
-    		os.remove(file_name)
+        if lock_sha256 != downloaded_sha256:
+            print('SHA256 did not match for lock.mdb')
+            print('Original SHA256: ' + data_sha256)
+            print('Download SHA256: ' + downloaded_sha256)
+            # RESTART JOB
+            restart_job()
+        else:
+            # checksum valid
+            print(file_name + " sha256sum is valid")
+            os.remove(file_name)
 
 
 
