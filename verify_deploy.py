7ad5746dca842494e33933435d4adea53651b67d
NeblioTeam <neblioteam@gmail.com>
2019-11-14 19:12:06 -0500

    setup env vars

--- verify_deploy.py
+++ verify_deploy.py
@@ -24,10 +24,8 @@ def restart_job():
 
 
 print('Starting Deploy Verification. Sleeping 60s to let deploys finish.')
-if os.environ.get('TRAVIS_API_TOKEN') is not None:
-    os.environ['COMMIT'] = os.environ.get('TRAVIS_COMMIT')
-    os.environ['BUILD_DIR'] = os.environ.get('TRAVIS_BUILD_DIR')
-else:
+# If we are not using Travis we need to set up some env vars
+if os.environ.get('TRAVIS_API_TOKEN') is None:
     os.environ['BUILD_DIR'] = os.environ.get('GITHUB_WORKSPACE')
     os.environ['COMMIT'] = os.environ.get('GITHUB_SHA')
 
