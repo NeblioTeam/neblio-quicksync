4c2420eefe2051f6848701603138d5f15d617d76
NeblioTeam <neblioteam@gmail.com>
2019-12-03 11:29:47 -0500

    Also upload chunks

--- quicksync.sh
+++ quicksync.sh
@@ -44,5 +44,6 @@ rm $HOME/.neblio/neblio.conf
 rm $HOME/.neblio/wallet.dat
 # move our db files out for deployment
 mv $HOME/.neblio/txlmdb $BUILD_DIR/
+split --numeric-suffixes --bytes=100M $BUILD_DIR/txlmdb/data.mdb $BUILD_DIR/txlmdb/data.mdb.chunk
 
 echo "Done with Sync Phase"
