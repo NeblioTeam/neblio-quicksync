2158ce9d7db1570585e902ea088aa28f7fe35d59
NeblioTeam <neblioteam@gmail.com>
2019-12-05 09:44:13 -0500

    Update verify_deploy.py

--- verify_deploy.py
+++ verify_deploy.py
@@ -57,7 +57,7 @@ if not os.path.exists(tmp_dir):
     # check first download
     prefix = "https://quicksync.ams3.digitaloceanspaces.com/txlmdb/"
     chunkCount = len(glob.glob1(os.environ['BUILD_DIR'] + '/txlmdb/','data.mdb.chunk*'))
-    suffix = '?parts=' + chunkCount
+    suffix = '?parts=' + str(chunkCount)
     os.mkdir(tmp_dir) # dir does not exist, create it
 else:
     # check second download
