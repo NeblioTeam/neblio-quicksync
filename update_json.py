47e5f0a068e246781dc447a95d4fc0c6c4d6a8ba
NeblioTeam <neblioteam@gmail.com>
2020-04-18 15:50:14 -0400

    Update update_json.py

--- update_json.py
+++ update_json.py
@@ -8,7 +8,7 @@ if os.environ.get('TRAVIS_API_TOKEN') is None:
 
 filename = 'download.json'
 # get the number of chunks from our previously created file
-chunkCount = glob.glob(os.environ['BUILD_DIR'] + '/txlmdb/chunks.*')[0].split('.chunks')[1]
+chunkCount = glob.glob(os.environ['BUILD_DIR'] + '/txlmdb/chunks.*')[0].split('chunks.')[1]
 parts = '/parts=' + str(chunkCount-1)
 with open(filename, 'r') as f:
     data = json.load(f)
